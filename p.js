b8868ed0c8bc23698fe7d2383dcfb3871197cb59
HExplorer07 <155760506+HExplorer07@users.noreply.github.com>
2024-01-11 19:04:48 +0530

    Update p.js

--- p.js
+++ p.js
@@ -142,14 +142,3 @@ darkModeIcon.onclick = () => {
 };
 
 
-ScrollReveal({
-    // reset: true,
-    distance: '60px',
-    duration: 2000,
-    delay: 100
-});
-
-ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
-ScrollReveal().reveal('.home-img img, .services-container, .portfolio-box, .testimonial-wrapper, .contact form', { origin: 'bottom' });
-ScrollReveal().reveal('.home-content h1, .about-img img', { origin: 'left' });
-ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', { origin: 'right' });
